b34d97150c291d8ca5d13a057430b01a0cc333c2
sehr <sehr.sehr1112@gmail.com>
2023-07-07 14:49:35 +0500

    New updation

--- Javascript-projects/Marks_calculation/script.js
+++ Javascript-projects/Marks_calculation/script.js
@@ -1,96 +1,87 @@
 // var stdrecord;
 
-   
-function btnclick(){   
-    event.preventDefault(); // Prevent form submission
-    var fscMarks;
-    var matricMarks;
-    var entryTestMarks;
-    var name = document.getElementById("name").value;
-    var rollNumber = document.getElementById("rollnmbr").value;
-    var totalMatricMarks = 1100;
-    var totalFscMarks = 1100;
-    var totalEntryTestMarks = 400;
-   
-    if((document.getElementById("matricmarks").value)> totalMatricMarks || (document.getElementById("matricmarks").value)<0)
-    {
-        alert("Matric Marks are invaid");
-    }
-    else{
-         matricMarks = document.getElementById("matricmarks").value;
+function btnclick() {
+  event.preventDefault(); // Prevent form submission
+  var fscMarks = document.getElementById("fscmarks").value;
+  var matricMarks = document.getElementById("matricmarks").value;
+  var entryTestMarks = document.getElementById("entrymarks").value;
+  var name = document.getElementById("name").value;
+  var rollNumber = document.getElementById("rollnmbr").value;
+  var totalMatricMarks = 1100;
+  var totalFscMarks = 1100;
+  var totalEntryTestMarks = 400;
 
-    }
-    if((document.getElementById("fscmarks").value)>totalFscMarks || (document.getElementById("fscmarks").value)<0)
-    {
-        alert("Fsc Marks are invaid");
-    }
-    else{
-         fscMarks = document.getElementById("fscmarks").value;
+  if (matricMarks > totalMatricMarks || matricMarks < 0) {
+    alert("Matric Marks are invaid");
+    matricMarks = 0;
+  }
 
-    }
-    if((document.getElementById("entrymarks").value)>totalEntryTestMarks || (document.getElementById("entrymarks").value)<0)
-    {
-        alert("Entry Test Marks are invaid");
-    }
-    else{
-         entryTestMarks = document.getElementById("entrymarks").value;
+  if (fscMarks > totalFscMarks || fscMarks < 0) {
+    alert("Fsc Marks are invaid");
+    fscMarks = 0;
+  }
 
-    }
+  if (entryTestMarks > totalEntryTestMarks || entryTestMarks < 0) {
+    alert("Entry Test Marks are invaid");
+    entryTestMarks = 0;
+  }
 
-    // var std = new Student(name,rollNumber,matricMarks,fscMarks,entryTestMarks);
-    // console.log(std);
-    // localStorage.setItem('std',JSON.stringify(std));
- 
-    var resultMessage = "<strong> Name: </strong>" + name + "<br>";
-    resultMessage += "<strong> Roll Number: </strong>" + rollNumber + "<br>";
-    resultMessage += "<strong> Matric Marks: </strong>" + matricMarks + "<br>";
-    resultMessage += "<strong> FSC Marks: </strong>" + fscMarks + "<br>";
-    resultMessage += " <strong> Entry Test Marks: </strong>" + entryTestMarks;
+  // var std = new Student(name,rollNumber,matricMarks,fscMarks,entryTestMarks);
+  // console.log(std);
+  // localStorage.setItem('std',JSON.stringify(std));
 
-    // Display the result on the screen
-    document.getElementById("result").innerHTML = resultMessage;
+  var resultMessage = "<strong> Name: </strong>" + name + "<br>";
+  resultMessage += "<strong> Roll Number: </strong>" + rollNumber + "<br>";
+  resultMessage += "<strong> Matric Marks: </strong>" + matricMarks + "<br>";
+  resultMessage += "<strong> FSC Marks: </strong>" + fscMarks + "<br>";
+  resultMessage += " <strong> Entry Test Marks: </strong>" + entryTestMarks;
 
+  // Display the result on the screen
+  document.getElementById("result").innerHTML = resultMessage;
 
-    
-   
-    var matricPercentage = (matricMarks / totalMatricMarks) * 100;
-    var fscPercentage = (fscMarks / totalFscMarks) * 100;
-    var entryTestPercentage = (entryTestMarks / totalEntryTestMarks) * 100;
+  var matricPercentage = (matricMarks / totalMatricMarks) * 100;
+  var fscPercentage = (fscMarks / totalFscMarks) * 100;
+  var entryTestPercentage = (entryTestMarks / totalEntryTestMarks) * 100;
 
-    var aggregate = (matricPercentage * 0.2) + (fscPercentage * 0.3) + (entryTestPercentage * 0.5);
-    var msg="<strong> Aggregate is: </strong>";
-    document.getElementById("aggregate").innerHTML = aggregate.toFixed(2);
-    document.getElementById("aggregate").innerHTML =msg + aggregate.toFixed(2) ;
+  var aggregate =
+    matricPercentage * 0.2 + fscPercentage * 0.3 + entryTestPercentage * 0.5;
+  var msg = "<strong> Aggregate is: </strong>";
+  document.getElementById("aggregate").innerHTML = aggregate.toFixed(2);
+  document.getElementById("aggregate").innerHTML = msg + aggregate.toFixed(2);
 
-    var resultElement = document.getElementById('container2');
+  var resultElement = document.getElementById("container2");
 
-    resultElement.innerHTML = "<strong> Aggregate: </strong> " + aggregate.toFixed(2);
+  resultElement.innerHTML =
+    "<strong> Aggregate: </strong> " + aggregate.toFixed(2);
 
- 
-    resultElement.innerHTML = "<strong> Student Name: </strong> " + name + "<br><strong> Aggregate: </strong> " + aggregate.toFixed(2);
+  resultElement.innerHTML =
+    "<strong> Student Name: </strong> " +
+    name +
+    "<br><strong> Aggregate: </strong> " +
+    aggregate.toFixed(2);
 
-    if ((aggregate) >= 60) {
-        resultElement.style.color = 'green';
-        resultElement.innerHTML += "<br><br><strong>Congratulations!</strong> You are eligible for admission.";
-    } else {
-        resultElement.style.color = 'red';
-        resultElement.innerHTML += "<br><br>Sorry, you are not eligible for admission.";
-    }
+  if (aggregate >= 60) {
+    resultElement.style.color = "green";
+    resultElement.innerHTML +=
+      "<br><br><strong>Congratulations!</strong> You are eligible for admission.";
+  } else {
+    resultElement.style.color = "red";
+    resultElement.innerHTML +=
+      "<br><br>Sorry, you are not eligible for admission.";
+  }
 
-    resultElement.style.display = 'block';
+  resultElement.style.display = "block";
 
+  var stdData = { name: name, aggregate: aggregate.toFixed(2) };
+  var array = [];
+  array.push(stdData);
+  localStorage.setItem("array", JSON.stringify(array));
+  console.log(array);
+  console.log(localStorage.getItem("array"));
 
-    let stdData = { name: name,
-        aggregate: aggregate.toFixed(2)}
-        var array=[];
-    array.push(stdData);
-    localStorage.setItem("array",JSON.stringify(array));
-    console.log(array);
+  //document.getElementById("myForm").reset();
   
-    //document.getElementById("myForm").reset();
-
-};
-
+}
 
 // class Student{
 //     constructor(name,rollNumber,matricMarks,fscMarks,entryTestMarks)
@@ -102,14 +93,28 @@ function btnclick(){
 //         this.entryTestMarks=entryTestMarks;
 //     }
 // }
-function stdRecord(){
-    //  record=(localStorage.getItem(std));
-    // document.getElementById("record").innerHTML = record;
-    window.location.href = 'record.html';
+function stdRecord() {
+  //  record=(localStorage.getItem(std));
+  // document.getElementById("record").innerHTML = record;
 
+  window.location.href = "record.html";
    
- 
-    
+  $(document).ready(function() {
 
-    
+    document.getElementById("aggregatesContainer").innerHTML="<p style='background-color:blue'>"+"new_data"+"</p><br/>";
+
+    var new_data = stdData;
+    document.getElementById("aggregatesContainer").innerHTML="<p style='background-color:blue'>"+new_data+"</p><br/>";
+    console.log(new_data)
+    if(localStorage.getItem('array')== null)
+    {
+      localStorage.setItem('array','[]');
+    }
+    var old_data = JSON.parse(localStorage.getItem('array'));
+    old_data.push(new_data);
+    localStorage.setItem('array',JSON.stringify(old_data));
+    document.getElementById("aggregatesContainer").innerHTML="<p style='background-color:blue'>"+JSON.parse(localStorage.getItem('array'))+"</p><br/>";
+  
+      });
+ 
 }
